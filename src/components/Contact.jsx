fef35fb33dfd9a3191688b39398aa476a49c58a1
arrpitgupta <arpit17200@gmail.com>
2025-02-19 07:55:05 +0530

    added email js

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,10 +7,10 @@ import emailjs from "@emailjs/browser";
 
 export default function Contact() {
   const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
+    name: "",
+    // lastName: "",
     email: "",
-    phone: "",
+    // phone: "",
     message: "",
   });
 
@@ -27,15 +27,15 @@ export default function Contact() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const serviceID = "YOUR_SERVICE_ID";
-    const templateID = "YOUR_TEMPLATE_ID";
-    const userID = "YOUR_USER_ID";
+    const serviceID = "service_f0hwc7g";
+    const templateID = "template_f9bijln";
+    const userID = "zD1A0xxF9ewiMPL9u";
 
     const emailParams = {
-      firstName: formData.firstName,
-      lastName: formData.lastName,
+      name: formData.name,
+      // lastName: formData.lastName,
       email: formData.email,
-      phone: formData.phone,
+      // phone: formData.phone,
       message: formData.message,
     };
 
@@ -47,11 +47,14 @@ export default function Contact() {
         (response) => {
           console.log("Email sent successfully!", response);
           setStatus({ success: true, message: "Message sent successfully!" });
-          setFormData({ firstName: "", lastName: "", email: "", phone: "", message: "" });
+          setFormData({ name: "", email: "", message: "" });
         },
         (error) => {
           console.error("Failed to send email:", error);
-          setStatus({ success: false, message: "Failed to send message. Please try again." });
+          setStatus({
+            success: false,
+            message: "Failed to send message. Please try again.",
+          });
         }
       )
       .finally(() => {
@@ -66,36 +69,72 @@ export default function Contact() {
           <Col xs={12} md={6}>
             <TrackVisibility>
               {({ isVisible }) => (
-                <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact Us" />
+                <img
+                  className={
+                    isVisible ? "animate__animated animate__zoomIn" : ""
+                  }
+                  src={contactImg}
+                  alt="Contact Us"
+                />
               )}
             </TrackVisibility>
           </Col>
           <Col xs={12} md={6}>
             <TrackVisibility>
               {({ isVisible }) => (
-                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
                   <h2>Get In Touch</h2>
                   <form onSubmit={handleSubmit}>
                     <Row>
                       <Col xs={12} sm={6} className="px-1">
-                        <input type="text" value={formData.firstName} placeholder="First Name" name="firstName" onChange={(e) => onFormUpdate("firstName", e.target.value)} />
+                        <input
+                          type="text"
+                          value={formData.name}
+                          placeholder="Name"
+                          name="name"
+                          onChange={(e) => onFormUpdate("name", e.target.value)}
+                        />
                       </Col>
-                      <Col xs={12} sm={6} className="px-1">
+                      {/* <Col xs={12} sm={6} className="px-1">
                         <input type="text" value={formData.lastName} placeholder="Last Name" onChange={(e) => onFormUpdate("lastName", e.target.value)} />
-                      </Col>
+                      </Col> */}
                       <Col xs={12} sm={6} className="px-1">
-                        <input type="email" value={formData.email} placeholder="Email Address" onChange={(e) => onFormUpdate("email", e.target.value)} />
+                        <input
+                          type="email"
+                          value={formData.email}
+                          placeholder="Email Address"
+                          name="email"
+                          onChange={(e) =>
+                            onFormUpdate("email", e.target.value)
+                          }
+                        />
                       </Col>
-                      <Col xs={12} sm={6} className="px-1">
+                      {/* <Col xs={12} sm={6} className="px-1">
                         <input type="tel" value={formData.phone} placeholder="Phone No." onChange={(e) => onFormUpdate("phone", e.target.value)} />
-                      </Col>
+                      </Col> */}
                       <Col xs={12} className="px-1">
-                        <textarea rows="6" value={formData.message} placeholder="Message" onChange={(e) => onFormUpdate("message", e.target.value)}></textarea>
-                        <button type="submit"><span>{buttonText}</span></button>
+                        <textarea
+                          rows="6"
+                          value={formData.message}
+                          placeholder="Message"
+                          name="message"
+                          onChange={(e) =>
+                            onFormUpdate("message", e.target.value)
+                          }
+                        ></textarea>
+                        <button type="submit">
+                          <span>{buttonText}</span>
+                        </button>
                       </Col>
                       {status && (
                         <Col>
-                          <p className={status.success ? "success" : "danger"}>{status.message}</p>
+                          <p className={status.success ? "success" : "danger"}>
+                            {status.message}
+                          </p>
                         </Col>
                       )}
                     </Row>
